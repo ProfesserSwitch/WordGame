6e09c323b944755fa7b529d3c34164f8fb821c07
manitchaya2004 <manitchaya.b@ku.th>
2025-12-17 14:42:43 +0700

    route path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route,} from "react-router-dom";
 import GameCanvas from "./BattlePage/walk";
 import HomePage from "./HomePage";
+import RegisterPage from "./RegisterPage";
 import GameAppBar from "../components/AppBar";
-
+import LoginPage from "./LoginPage";
 export default function App() {
-  const location = useLocation();
 
-  // ซ่อน AppBar เฉพาะหน้า /battle
-  const hideAppBar = location.pathname === "/battle";
 
   return (
     <>
       {/* {!hideAppBar && <GameAppBar />} */}
 
       <Routes>
+        {/* <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage/>}/> */}
         <Route path="/" element={<HomePage />} />
         <Route path="/battle" element={<GameCanvas />} />
       </Routes>
