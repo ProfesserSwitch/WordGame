71cbf90c0b2328727628d4459499eb4516a21b23
manitchaya2004 <manitchaya.b@ku.th>
2026-01-03 19:54:45 +0700

    เพิ่ม state เรียกใช้ข้อมูล ของ shop dictionary

--- src/pages/HomePage/hook/useData.ts
+++ src/pages/HomePage/hook/useData.ts
@@ -1,31 +1,69 @@
-import { useAppDispatch,useAppSelector } from "../../../hook/auth";
+import { useAppDispatch, useAppSelector } from "../../../hook/auth";
 import { searchShopItems, ClearShopItems } from "../../../store/reducers/shop";
+import {
+  clearDictionary,
+  fetchDictionary,
+} from "../../../store/reducers/dictionary";
 // import { getAllStage } from "../../../store/reducers/stage";
-// import { useCallback,useEffect } from "react";
+import { useCallback, useEffect } from "react";
 export const useData = () => {
-    const dispatch = useAppDispatch();
-    
-    const stages = useAppSelector((state) => state.stage.stages);
-    const shops = useAppSelector((state) => state.shop.allShops);
-    const item = useAppSelector((state) => state.shop.item);
-    const loading = useAppSelector((state) => state.shop.loading);
+  const dispatch = useAppDispatch();
 
-    const searchItems = (searchTerm: string) => {
-      dispatch(searchShopItems(searchTerm));
-    };
+  const stages = useAppSelector((state) => state.stage.stages);
+  const shops = useAppSelector((state) => state.shop.allShops);
+  const item = useAppSelector((state) => state.shop.item);
 
-    const resetItems = () => {
-      dispatch(ClearShopItems());
-    };
-    
+  //loading
+  const loading = useAppSelector((state) => state.shop.loading);
 
-    return {
-      stages,
-      shops,
-      item,
-      loading,
+  //stage
+  const loadingStage = useAppSelector((state) => state.stage.loading);
 
-      searchItems,
-      resetItems,
-    }
-}
+  // const loadin
+
+  //shop features
+  const searchItems = (searchTerm: string) => {
+    dispatch(searchShopItems(searchTerm));
+  };
+
+  const resetItems = () => {
+    dispatch(ClearShopItems());
+  };
+
+  // dictionary features
+  const dictionary = useAppSelector((state) => state.dictionary.words);
+  const DictionaryState = useAppSelector((state) => state.dictionary.loading);
+  const hasNext = useAppSelector((state) => state.dictionary.hasNext);
+  const lastWord = useAppSelector((state) => state.dictionary.lastWord)
+  const fetchDictionarys = useCallback(
+    (letter: string, limit: number) => {
+      dispatch(fetchDictionary({ startsWith: letter, limit: limit }));
+    },
+    [dispatch]
+  );
+
+  // const loginPlayer = useCallback(
+  //     (username: string, password: string) => {
+  //       return dispatch(loginUser({ username, password })).unwrap();
+  //     },
+  //     [dispatch]
+  //   );
+  return {
+    //shop
+    shops,
+    item,
+    loading,
+    searchItems,
+    resetItems,
+
+    //stages
+    stages,
+    loadingStage,
+   
+    //dictionary
+    dictionary,
+    DictionaryState,
+    hasNext,
+    lastWord
+  };
+};
